ed8b0a8be65adce30092e809a549011b6ea0890a
cyrus305 <cyrusescyrus@gmail.com>
2017-12-17 14:01:51 -0500

    Assignment Week2

--- src/app/app-routing/route.ts
+++ src/app/app-routing/route.ts
@@ -3,9 +3,11 @@ import {MenuComponent} from "../menu/menu.component";
 import {Routes} from "@angular/router";
 import {ContactComponent} from "../contact/contact.component";
 import {DishdetailComponent} from "../dishdetail/dishdetail.component";
+import {AboutComponent} from "../about/about.component";
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
   {path: 'menu', component: MenuComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'contactus', component: ContactComponent},
